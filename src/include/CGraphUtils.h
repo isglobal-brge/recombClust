89deacebd84c3d49d96d3cc1e8dfae6fd5c79203
dpelegri <dpelegri@gmail.com>
2021-02-11 16:48:16 +0100

    Update CGraphUtils.h

--- src/include/CGraphUtils.h
+++ src/include/CGraphUtils.h
@@ -4,8 +4,12 @@
    #include <RcppEigen.h>
 
 
-   Rcpp::RObject get_local_cores(Rcpp::NumericVector cores, int hrst);
-
+   //..// Rcpp::RObject get_local_cores(Rcpp::NumericVector cores, int hrst);
+   //..// std::map<int, int> get_local_cores(Rcpp::NumericVector cores, int hrst);
+   std::map<int, std::vector<int>> get_local_cores(Rcpp::NumericVector cores, int hrst);
+   
    Rcpp::RObject get_graph_matrix_data( Rcpp::RObject OCM, int hrstParam );
+   Rcpp::NumericVector get_localcores_map_as_vector(std::map<int, std::vector<int>> localCores);
+   
 
 #endif
